19eab8c056e04be1ab04477818dde7afbd1f2ad4
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2015-11-04 07:56:04 +1100

    Initial commit.

---
+++ IFAAppUI/IFAAppUI/IFAAppUI.h
@@ -0,0 +1,19 @@
+//
+//  IFAAppUI.h
+//  IFAAppUI
+//
+//  Created by Marcelo Schroeder on 4/11/2015.
+//  Copyright © 2015 InfoAccent Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IFAAppUI.
+FOUNDATION_EXPORT double IFAAppUIVersionNumber;
+
+//! Project version string for IFAAppUI.
+FOUNDATION_EXPORT const unsigned char IFAAppUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IFAAppUI/PublicHeader.h>
+
+
